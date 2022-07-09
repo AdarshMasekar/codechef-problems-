633ea7e8932bbbeb94e7012f3990cf0312cf52a6
Adarsh Masekar <108608608+AdarshMasekar@users.noreply.github.com>
2022-07-10 02:20:34 +0530

    Create biryani classes.cpp

---
+++ biryani classes.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int t;
+  cin>>t;
+  while(t--){
+    int x,y;
+    cin>>x>>y;   
+    // x = no.of weeks to attend classes 
+    // y = cost per week
+    cout<<x*y<<endl;
+  }
+}
+  

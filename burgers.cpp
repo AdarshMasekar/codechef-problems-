e94e6aa2910aa72317c41f61ffcd48dfb16ef598
Adarsh Masekar <108608608+AdarshMasekar@users.noreply.github.com>
2022-07-10 22:41:03 +0530

    Create burgers.cpp

---
+++ burgers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+     while(t--){
+        int p,b;
+        cin>>p>>b;
+        if(p<=b){
+            cout<<p<<endl;
+        }else{
+            cout<<b<<endl;
+        }
+     }
+}

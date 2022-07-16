804c2f085eab63a60a74439dad3828e80c8f2832
Adarsh Masekar <108608608+AdarshMasekar@users.noreply.github.com>
2022-07-16 21:52:23 +0530

    Create chess.cpp

---
+++ chess.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,a,ans;
+	    cin>>n;
+	    a=n*60;
+	    ans=a%20;
+	    cout<<ans<<endl;
+	    }
+	    
+	}
+	return 0;
+}
